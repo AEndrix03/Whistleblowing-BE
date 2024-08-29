bea0fd2654bd58d97ada4e08209cbeaa95007629
ARedegalli <aredegalli@omniagroup.it>
2024-08-29 09:55:17 +0200

    inizializzazione spring project da spring-initalizr

---
+++ src/main/java/it/omniaservice/whistleblowing/WhistleblowingApplication.java
@@ -0,0 +1,13 @@
+package it.omniaservice.whistleblowing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhistleblowingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhistleblowingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/omniaservice/whistleblowing/WhistleblowingApplicationTests.java
@@ -0,0 +1,13 @@
+package it.omniaservice.whistleblowing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhistleblowingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
